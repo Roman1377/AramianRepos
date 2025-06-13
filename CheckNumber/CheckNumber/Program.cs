ff310ea487798ad57efb757a7255822f62a04e0e
Roman <papa.apy@gmail.comgit config --global user.email papa.apy@gmail.com>
2025-06-13 13:28:32 +0200

    Added homework#2 (Numb checker)

---
+++ CheckNumber/CheckNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter a number:");
+
+string input = Console.ReadLine();
+
+try
+{   int inputNumb = int.Parse(input);
+    Console.WriteLine("You entered a valid number: " + inputNumb);
+}
+catch (FormatException)
+{
+    Console.WriteLine("Invalid input. Please enter a valid integer.");
+}
+finally
+{
+    Console.WriteLine("Block finally");
+}
