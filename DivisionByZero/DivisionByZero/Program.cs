d721de0c8253cdfdffe253192aee107f6d86c9ca
Roman <papa.apy@gmail.comgit config --global user.email papa.apy@gmail.com>
2025-06-13 12:38:24 +0200

    Added homework#1 (Division by zero)

---
+++ DivisionByZero/DivisionByZero/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Enter fist number:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+try
+{
+    int result = firstNumber / secondNumber;
+    Console.WriteLine($"Result: {result}");
+
+    if (secondNumber == 0)
+
+        Console.WriteLine("Error: Division by zero is not allowed.");
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine("Error: Division by zero is not allowed.");
+}
+
+finally
+{
+    Console.WriteLine("Execution completed.");
+}
